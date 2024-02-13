2ee513c61f4706a6c6f7508be01dc1b109758656
Karina Ting <khting@stanford.edu>
2024-02-12 19:21:15 -0800

    circle tracking from hw2 and spawning cubes in gazebo

---
+++ me326_locobot_example/launch/spawn_cube.launch.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    blue_urdf = os.path.join(get_package_share_directory('me326_locobot_example'), 'model/blue_cube.urdf')
+    red_urdf = os.path.join(get_package_share_directory('me326_locobot_example'), 'model/red_cube.urdf')
+    green_urdf = os.path.join(get_package_share_directory('me326_locobot_example'), 'model/green_cube.urdf')
+    yellow_urdf = os.path.join(get_package_share_directory('me326_locobot_example'), 'model/yellow_cube.urdf')
+
+    blue_cube_x = '0.5'
+    blue_cube_y = '0.0'
+    blue_cube_z = '0.1'
+    blue_cube_R = '0.0'
+    blue_cube_P = '0.0'
+    blue_cube_Y = '0.0'
+
+    red_cube_x = '0.5'
+    red_cube_y = '0.3'
+    red_cube_z = '0.1'
+    red_cube_R = '0.0'
+    red_cube_P = '0.0'
+    red_cube_Y = '0.0'
+
+    green_cube_x = '0.5'
+    green_cube_y = '-0.3'
+    green_cube_z = '0.1'
+    green_cube_R = '0.0'
+    green_cube_P = '0.0'
+    green_cube_Y = '0.0'
+
+    yellow_cube_x = '0.8'
+    yellow_cube_y = '0.0'
+    yellow_cube_z = '0.1'
+    yellow_cube_R = '0.0'
+    yellow_cube_P = '0.0'
+    yellow_cube_Y = '0.0'
+
+
+    return LaunchDescription([
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='spawn_blue_cube',
+            output='screen',
+            arguments=['-file', blue_urdf,
+                        '-entity', 'blue_cube',
+                        '-x', blue_cube_x,
+                        '-y', blue_cube_y,
+                        '-z', blue_cube_z,
+                        '-R', blue_cube_R,
+                        '-P', blue_cube_P,
+                        '-Y', blue_cube_Y,],
+        ),
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='spawn_red_cube',
+            output='screen',
+            arguments=['-file', red_urdf,
+                        '-entity', 'red_cube',
+                        '-x', red_cube_x,
+                        '-y', red_cube_y,
+                        '-z', red_cube_z,
+                        '-R', red_cube_R,
+                        '-P', red_cube_P,
+                        '-Y', red_cube_Y,],
+        ),
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='spawn_green_cube',
+            output='screen',
+            arguments=['-file', green_urdf,
+                        '-entity', 'green_cube',
+                        '-x', green_cube_x,
+                        '-y', green_cube_y,
+                        '-z', green_cube_z,
+                        '-R', green_cube_R,
+                        '-P', green_cube_P,
+                        '-Y', green_cube_Y,],
+        ),
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='spawn_yellow_cube',
+            output='screen',
+            arguments=['-file', yellow_urdf,
+                        '-entity', 'yellow_cube',
+                        '-x', yellow_cube_x,
+                        '-y', yellow_cube_y,
+                        '-z', yellow_cube_z,
+                        '-R', yellow_cube_R,
+                        '-P', yellow_cube_P,
+                        '-Y', yellow_cube_Y,],
+        )
+    ])
---
+++ me326_locobot_example/scripts/circle_motion_tracking.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+'''
+Written by: Karina Ting, Date: 2/1/2024
+
+This script starts at the bottom "if __name__ == '__main__':" which is a function that calls "main():" function.
+The main function then instanties a class object (Locobot_example), which takes in a target pose, then listens to the topic 
+of the robots odometry (pose), and then calculates the control action to take, then commands the velocity to the robot
+
+read more about rospy publishers/subscribers here: http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
+'''
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
+
+import numpy as np
+import scipy as sp
+from scipy import linalg
+import geometry_msgs
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Point
+from nav_msgs.msg import Odometry
+from visualization_msgs.msg import Marker
+
+
+class CircleTracking(Node):
+    """
+    """
+    def __init__(self):
+        super().__init__('locobot_circle_tracking')
+
+        self.t_init = None
+        self.L = 0.1 #this is the distance of the point P (x,y) that will be controlled for position. The locobot base_link frame points forward in the positive x direction, the point P will be on the positive x-axis in the body-fixed frame of the robot mobile base
+
+        self.target_pose = Pose()
+        self.r = 0.5  #radius of circle trajectory to track
+        self.w = 2*np.pi/10  # frequency of circular motion in rad/s
+        self.Kp = 2  #try 0.5, 1, 2
+
+        # Command Velocity Publisher
+        self.mobile_base_vel_publisher = self.create_publisher(Twist, "/locobot/diffdrive_controller/cmd_vel_unstamped", 10)
+        ## Original:
+        # self.mobile_base_vel_publisher = self.create_publisher(Twist, "/locobot/mobile_base/commands/velocity", 10)
+        ## For real robot: (?)
+        # self.mobile_base_vel_publisher = self.create_publisher(Twist, "/locobot/mobile_base/cmd_vel", 10)
+
+        # Base Odom Subscriber:
+        self.create_subscription(Odometry, "/odom", self.mobile_base_callback, 10) 
+        ## Original (for real robot?)
+        # self.create_subscription(Odometry, "/locobot/mobile_base/odom", self.mobile_base_callback, 10) 
+        ## Also works for sim but need to change qos profile:
+        # qos_profile = QoSProfile(
+        #     reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
+        #     history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+        #     depth=10
+        # )
+        # self.create_subscription(Odometry, "/locobot/sim_ground_truth_pose", self.mobile_base_callback, qos_profile=qos_profile) 
+
+
+    def mobile_base_callback(self, data):
+        """
+        This function is the callback for the message that is publishing the pose of mobile base.
+        Note: the locobot bases are non-holonomic, meaning that like a wheelchair, they cannot 
+        instantaneously move side-to-side, so to control the (x,y) position of the base, we must control a point (we will call P) 
+        that is in front or behind the non-holonomic line (the line that connects the center of the wheels). It turns out 
+        that it is possible to control this point in any direction we choose, but we cannot simultaneosuly control the position (x,y) and the planar angle (theta).
+        So the strategy will be to move to the desired (x,y) location, then rotate to the desired angle sequentially (in this script, we will focus on just the xy motion, and leave as an exercise the rotation to the student)
+
+        Note: this message topic /locobot/mobile_base/odom is published at about 30Hz, and we are using it to trigger our control script (so we control at the 
+        frequency we obtain the state information)
+        """
+        t = (self.get_clock().now().nanoseconds - self.t_init)*1e-9
+        self.target_pose.position.x = self.r*np.cos(t*self.w)
+        self.target_pose.position.y = self.r*np.sin(t*self.w)
+
+
+        # Step 1: Calculate the point P location (distance L on the x-axis), and publish the marker so it can be seen in Rviz
+        #first determine the relative angle of the mobile base in the world xy-plane, this angle is needed to determine where to put the point P
+        #the rotation will be about the world/body z-axis, so we will only need the qw, and qz quaternion components. We can then use knoweldge of the 
+        #relationship between quaternions and rotation matricies to see how we must rotate the Lx vector into the world (odom) frame and add it to the base position
+        #to obtain the point P (for more info on quaterions, see a primer at the bottom of this page: https://arm.stanford.edu/resources/armlab-references)
+        qw = data.pose.pose.orientation.w
+        qx = data.pose.pose.orientation.x
+        qy = data.pose.pose.orientation.y
+        qz = data.pose.pose.orientation.z
+        R11 = qw**2 + qx**2 - qy**2 -qz**2
+        R12 = 2*qx*qz - 2*qw*qz
+        R21 = 2*qx*qz + 2*qw*qz
+        R22 = qw**2 - qx**2 + qy**2 -qz**2
+
+        point_P = Point()
+        #NOTE: the following assumes that when at the origin, the baselink and odom/world frame are aligned, and the z-axis points up. If this is not true then there is not a simple rotation about the z-axis as shown below
+        point_P.x = data.pose.pose.position.x + self.L*R11
+        point_P.y = data.pose.pose.position.y + self.L*R21
+        point_P.z = 0.1  #make it hover just above the ground (10cm)
+
+
+        # Step 2: Calculate the error between the target pose for position control (this will relate to the proportoinal gain matrix, the P in PID control)
+        err_x = self.target_pose.position.x - point_P.x
+        err_y = self.target_pose.position.y - point_P.y
+        error_vect = np.matrix([[err_x],[err_y]]) #this is a column vector (2x1); equivalently, we could use the transpose operator (.T): np.matrix([err_x ,err_y]).T  
+
+        Kp_mat = self.Kp*np.eye(2) #proportional gain matrix, diagonal with gain of 0.2 (for PID control)
+
+        #We will deal with this later (once we reached the position (x,y) goal), but we can calculate the angular error now - again this assumes there is only planar rotation about the z-axis, and the odom/baselink frames when aligned have x,y in the plane and z pointing upwards
+        Rotation_mat = np.matrix([[R11,R12],[R21,R22]])
+        #We can use matrix logarithm (inverse or Rodrigues Formula and exponential map) to get an axis-angle representation:
+        axis_angle_mat = sp.linalg.logm(Rotation_mat)
+        
+        # This is the angle error: how should frame Base move to go back to world frame?
+        angle_error = axis_angle_mat[0,1] #access the first row, second column to get angular error (skew sym matrix of the rotation axis - here only z component, then magnitude is angle error between the current pose and the world/odom pose which we will return to both at points A and B) 
+        
+        # This Angle is selected because its the frame rotation angle, how does Base appear from world?
+        current_angle = axis_angle_mat[1,0] #this is also the angle about the z-axis of the base
+        Kp_angle_err = 0.2 #gain for angular error (here a scalar because we are only rotating about the z-axis)
+
+
+        '''
+        We do not do perform derivative control here because we are doing velocity control, 
+        and an input of velocity is feedforward control, not feedback (same derivative order as input is feedforward)
+        Since we are tracking a point B, we will just use PI (proportional, integral). 
+        But if a trajectory were specified (we were told at each time, what the position, velocity should be) 
+        then we could use feedforward control, and if we were controlling acceleration as oppose to velocity, 
+        then we could use the derivative component of PID (acceleration is 2nd order control, velocity is 1st order derivative)
+        '''        
+
+        # Step 3: now put it all together to calculate the control input (velocity) based on the position error and integrated error
+        point_p_error_signal = Kp_mat*error_vect #+ Ki_mat*pos_err_integrated
+        #The following relates the desired motion of the point P and the commanded forward and angular velocity of the mobile base [v,w]
+        non_holonomic_mat = np.matrix([[np.cos(current_angle), -self.L*np.sin(current_angle)],[np.sin(current_angle),self.L*np.cos(current_angle)]])
+        #Now perform inversion to find the forward velocity and angular velcoity of the mobile base.
+        control_input = np.linalg.inv(non_holonomic_mat)*point_p_error_signal #note: this matrix can always be inverted because the angle is L
+        #now let's turn this into the message type and publish it to the robot:
+        control_msg = Twist()
+        control_msg.linear.x = float(control_input.item(0)) #extract these elements then cast them in float type
+        control_msg.angular.z = float(control_input.item(1))
+
+        #now publish the control output:
+        self.mobile_base_vel_publisher.publish(control_msg)
+
+        #find the magnitude of the positional error to determine if its time to focus on orientation or switch targets
+        err_magnitude = np.linalg.norm(error_vect)
+
+
+    def go_to_pose(self, target_pose=None):
+        """
+        Input: Target_pose - ROS geometry_msgs.msg.Pose type
+        To see what it looks like, put the following in the terminal: $ rosmsg show geometry_msgs/Pose
+        """
+        self.target_pose.position.x = self.r*np.cos(0.0)
+        self.target_pose.position.y = self.r*np.sin(0.0)
+        self.t_init = self.get_clock().now().nanoseconds
+
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CircleTracking()
+    node.go_to_pose()
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    #this is where the script begins, calls the main function
+    main()
+
